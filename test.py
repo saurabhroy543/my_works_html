37dd72722d787f6e11633ebcbea991fddd34b09d
shubham <saurabhroy543@gmail.com>
2021-04-05 12:58:00 +0530

    modified code py

--- test.py
+++ test.py
@@ -1,8 +1,11 @@
+'''Amazon question 2. find the id's of the 2 list element in which
+ one is id and 2nd is the useage of target ,so find all the pairs of
+ id's from a and b which uses the maximum of target'''
+
 a = [[1, 8], [2, 15], [3, 9]]
 b = [[1, 8], [2, 11], [3, 12]]
 target = 20
 c=[]
-count=0
 d=[]
 e=[]
 for i in a:
@@ -10,12 +13,9 @@ for i in a:
         if i[1]+j[1]<=target:
             sum=i[1]+j[1]
             c.append(sum)
-            id1=i[0]
-            id2=j[0]
             d.append([i[0],j[0]])
 m=max(c)
 for i in c:
-
     if i==m:
         e.append(d[count])
     count=count+1
